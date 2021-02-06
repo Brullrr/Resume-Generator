c09041c44b039bc04b27ef9fc703e5e327c6de1e
Brullrr <rbrull@yahoo.com>
2021-02-06 10:37:37 +0900

    Attempts at fixing router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-
       <App />
     </BrowserRouter>
     
