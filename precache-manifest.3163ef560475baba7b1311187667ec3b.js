4c1a9e65d21ffb03d206ac1e260327f46280a18d
Brullrr <rbrull@yahoo.com>
2021-02-06 10:10:50 +0900

    Updates

---
+++ precache-manifest.3163ef560475baba7b1311187667ec3b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77619f0f9124d4e9246fe5f852bb957",
+    "url": "/Resume-Generator/index.html"
+  },
+  {
+    "revision": "6bdc1996164dfbb65145",
+    "url": "/Resume-Generator/static/css/main.db014755.chunk.css"
+  },
+  {
+    "revision": "ee7bddfe9309fc4b72b8",
+    "url": "/Resume-Generator/static/js/2.479b04fc.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/Resume-Generator/static/js/2.479b04fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e6032c5f2180f83910",
+    "url": "/Resume-Generator/static/js/3.6def22c3.chunk.js"
+  },
+  {
+    "revision": "0ffb710dceef036264c25d24acb382eb",
+    "url": "/Resume-Generator/static/js/3.6def22c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ef761d2055b0ba888d",
+    "url": "/Resume-Generator/static/js/4.e253411d.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/Resume-Generator/static/js/4.e253411d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f371773ddee69f4b4e18",
+    "url": "/Resume-Generator/static/js/5.7c22d1aa.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/Resume-Generator/static/js/5.7c22d1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bdc1996164dfbb65145",
+    "url": "/Resume-Generator/static/js/main.3428c2d3.chunk.js"
+  },
+  {
+    "revision": "bfbd868758b278c2449e",
+    "url": "/Resume-Generator/static/js/runtime-main.a8c1a4e2.js"
+  },
+  {
+    "revision": "0196669f946facfc9ad477df48036815",
+    "url": "/Resume-Generator/static/media/earthLogo.0196669f.png"
+  },
+  {
+    "revision": "98160c661f3b8adb42d82b3d5191b72b",
+    "url": "/Resume-Generator/static/media/office city view.98160c66.jpg"
+  },
+  {
+    "revision": "84e1790924704b0c04191672dc7c8d24",
+    "url": "/Resume-Generator/static/media/office meeting room.84e17909.jpg"
+  },
+  {
+    "revision": "f69ce41d2d683f8edc92f2877a51fcde",
+    "url": "/Resume-Generator/static/media/office plant.f69ce41d.jpg"
+  }
+]);
