8a86aa52406cef7573d9c2b13fcbc1dbc03c4e8f
Brullrr <rbrull@yahoo.com>
2021-03-03 23:21:01 +0900

    attemp at 404 error fix 2

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <HashRouter>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
+    </HashRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
